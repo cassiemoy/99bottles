59384b12a8f651b16e6e27262f5c5ce60f1bc580
Cassie Moy <hello@cassie.nyc>
2019-07-15 15:58:06 -0400

    Add pronoun for bottle.

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -3,11 +3,19 @@ class Bottles
     new_count = count - 1
     
     <<~VERSE
-      #{count} bottles of beer on the wall, #{count} bottles of beer.
-      Take one down and pass it around, #{new_count} #{pluralize_bottles(new_count)} of beer on the wall.
+      #{count} #{pluralize_bottles(count)} of beer on the wall, #{count} #{pluralize_bottles(count)} of beer.
+      Take #{pronoun(count)} down and pass it around, #{quantitative_adjective(new_count)} #{pluralize_bottles(new_count)} of beer on the wall.
     VERSE
   end
 
+  def pronoun(count)
+    count == 1 ? "it" : "one"
+  end
+
+  def quantitative_adjective(count)
+    count == 0 ? "no more" : "#{count}"
+  end
+  
   def pluralize_bottles(count)
     count == 1 ? "bottle" : "bottles"
   end
