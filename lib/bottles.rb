6c2c54052139b2bbd6bed6f73a0af47c04faffd1
Cassie Moy <hello@cassie.nyc>
2019-07-15 14:47:45 -0400

    Add bottle pluralization to pass 3rd test.

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,8 +1,14 @@
 class Bottles
-  def verse(total_bottles)
+  def verse(count)
+    new_count = count - 1
+    
     <<~VERSE
-      #{total_bottles} bottles of beer on the wall, #{total_bottles} bottles of beer.
-      Take one down and pass it around, #{total_bottles - 1} bottles of beer on the wall.
+      #{count} bottles of beer on the wall, #{count} bottles of beer.
+      Take one down and pass it around, #{new_count} #{pluralize_bottles(new_count)} of beer on the wall.
     VERSE
-	end
+  end
+
+  def pluralize_bottles(count)
+    count == 1 ? "bottle" : "bottles"
+  end
 end
