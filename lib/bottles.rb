515c61be5ba3affb3c49674af5b89d4a5657611c
Cassie Moy <hello@cassie.nyc>
2019-07-17 21:32:07 -0400

    Round, not count.

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -13,31 +13,31 @@ class Bottles
     verses.join("\n")
   end
 
-  def verse(count)  
-    first_line =  "#{quantitative_adjective(count)} #{pluralize_bottles(count)} of beer on the wall, #{quantitative_adjective(count)} #{pluralize_bottles(count)} of beer.\n"
+  def verse(round)  
+    first_line =  "#{quantitative_adjective(round)} #{pluralize_bottles(round)} of beer on the wall, #{quantitative_adjective(round)} #{pluralize_bottles(round)} of beer.\n"
     
-    second_line = get_second_line(count)
+    second_line = get_second_line(round)
 
     [first_line, second_line].map(&:capitalize).join
   end
 
-  def get_second_line(count)
-    if count == 0
+  def get_second_line(round)
+    if round == 0
       "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
     else
-      "Take #{pronoun(count)} down and pass it around, #{quantitative_adjective(count - 1)} #{pluralize_bottles(count - 1)} of beer on the wall.\n"
+      "Take #{pronoun(round)} down and pass it around, #{quantitative_adjective(round - 1)} #{pluralize_bottles(round - 1)} of beer on the wall.\n"
     end
   end
   
-  def pronoun(count)
-    count == 1 ? "it" : "one"
+  def pronoun(round)
+    round == 1 ? "it" : "one"
   end
 
-  def quantitative_adjective(count)
-    count == 0 ? "no more" : "#{count}"
+  def quantitative_adjective(round)
+    round == 0 ? "no more" : "#{round}"
   end
   
-  def pluralize_bottles(count)
-    count == 1 ? "bottle" : "bottles"
+  def pluralize_bottles(round)
+    round == 1 ? "bottle" : "bottles"
   end
 end
